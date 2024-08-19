964e3a44cf78af39d89cec5d85b799ca7622aa48
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-08-19 16:04:39 +0400

    Add files via upload

---
+++ Day 1/Lesson 1.py
@@ -0,0 +1,39 @@
+name = "saba"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "saba" არის ცვლადის მნიშვნელობა
+
+surname = "latsabidze"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "saba" #ეს არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int ( integer ) მთელი რიცხვი
+height = 175.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLean (booL) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False  #snakecase ( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიპტული cameLcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + surname)
+
+print(str(age))
+
+print(height)
+
+print("may14")
