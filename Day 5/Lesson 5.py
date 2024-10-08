165e545e61f0866ccbf48b708594514800148b42
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-10-08 18:13:25 +0400

    Add files via upload

---
+++ Day 5/Lesson 5.py
@@ -0,0 +1,31 @@
+#input() ფუნქცია
+
+#input () არის ტექსტური მონაცემთა ტიპი
+#num1= int (input ("ჩაწერე ციფრი1:"))
+#num2= int (input ("ჩაწერე ციფრი2:"))
+
+#print (float("1")+1)
+#print (float("1"+ 1))
+
+#name = (input (6+8))
+#surname = (input(5+5))
+
+#-,
+#+,
+#/,გაყოფა float
+#//,  გაყოფა int
+#*, გამრავლება
+#**, ხარისხი
+#%, პროცენტი
+
+#print("float", 15/4)
+#print("int", 15//4)
+
+#print("** ნიშანი", 5**3)
+#print("gamravleba", 5 * 5 * 5)
+#print(7 % 3)
+
+# type გვეუბნება რა მონაცემთა ტიპია
+#print( type(input()))
+
+#name = input("")
