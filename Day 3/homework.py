6f171be477125b8f2ddbc9bfd434bf5e658e1dc6
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-10-13 16:03:35 +0400

    Add files via upload

---
+++ Day 3/homework.py
@@ -0,0 +1,17 @@
+name = ("saba")
+lastname = ("latsabidze")
+age = ("14")
+academy = (input("Goa"))
+school = (input("159"))
+
+print (input(name))
+print (input(lastname))
+print (input(age))
+print (input(academy))
+print (input(school))
+
+user = (input("sabalatsabidze"))
+print ("user")
+
+
+
