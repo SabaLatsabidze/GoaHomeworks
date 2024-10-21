395d27a8244123325dca2a69dc8ee6f99cb9f50b
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-10-21 14:38:19 +0400

    Add files via upload

---
+++ Day 12/Homework.py
@@ -0,0 +1,16 @@
+#მართკუთხედი
+width = 15
+height = 4  
+
+for i in range(height):
+    print('*' * width)
+
+
+print ("----------------")
+
+#კვადრატი
+width = 8   
+height = 4  
+
+for i in range(height):
+    print('*' * width)
