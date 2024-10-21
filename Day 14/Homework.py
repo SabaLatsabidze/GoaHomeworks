569afe6e34eebdd067ff9f84d24fe472ca254109
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-10-21 14:29:30 +0400

    Add files via upload

---
+++ Day 14/Homework.py
@@ -0,0 +1,13 @@
+
+name=input("What is your name?: ")
+age=input("how old are you?: ")
+
+print("Hello "+ name)
+print(name + "--" "you are" "--" + age)
+
+
+for number in range(-100, 100):
+    if number > 0:
+        print(number)
+
+
