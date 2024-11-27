b0d437d34068f7feed44fc0a0ac6619c0b5f8ee5
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-11-27 19:33:36 +0400

    Delete Day 22/Homework directory

--- Day 22/Homework/Homework.py
+++
@@ -1,8 +0,0 @@
-list_of_names = ["Saba", "Mariami", "Nika", "Davit", "Giorgi", "Saba"]
-
-
-name_to_count = "Saba"
-count = list_of_names.count(name_to_count)
-
-
-print(f"სახელი '{name_to_count}' მეორდება {count} ჯერ.")
