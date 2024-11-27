a52975882e499d6e5a81933158773e9df75e6f43
SabaLatsabidze <sabalatsabidze59@gmail.com>
2024-11-27 19:43:47 +0400

    Add files via upload

---
+++ Day 17/Homework.py
@@ -0,0 +1,12 @@
+
+favorite_fruits = ["ვაშლი", "ბანანი", "საზამთრო", "მსხალი", "ბროწეული"]
+
+
+first_fruit = favorite_fruits[0]
+
+
+last_fruit = favorite_fruits[-1]
+
+
+print("პირველი ხილი:", first_fruit)
+print("ბოლო ხილი:", last_fruit)
---
+++ Day 17/Lesson.py
@@ -0,0 +1,11 @@
+
+favorite_Bodybuilders = ["Levrone", "Coleman", "Zyzz", "Arnold"]
+
+first_Bodybuilder = favorite_Bodybuilders[0]
+
+
+last_BOdybuilder = favorite_Bodybuilders[-1]
+
+
+print("First bodybuilder:", first_Bodybuilder)
+print("Last bodybuilder:", last_BOdybuilder)
